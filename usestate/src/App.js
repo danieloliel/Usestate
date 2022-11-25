1865aecbb59fa4f74892b796d54395ed3b08526a
Daniel Oliel <danielol@post.jce.ac.il>
2022-11-25 11:50:08 +0200

    ReFactoring Process

--- usestate/src/App.js
+++ usestate/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 
 import Counter from './Counter';
+import { useState } from 'react';
+
+
 function App() {
+  const [delta, setDelta] = useState(1); 
+function handleDelta(event){
+  console.log(event)
+  setDelta(Number(event.target.value))
+}   
   return (
     <div className="App">
      <h1 >UseState</h1>
+     <p><input type="number" value = {delta} onChange= {handleDelta}/></p>
       <p></p>
-     <Counter />
+     <Counter delta= {delta} />
+     <Counter delta= {delta} />
     </div>
   );
 }
--- usestate/src/Counter.js
+++ usestate/src/Counter.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
-function Counter() {
+function Counter(props) {
+    const {delta} = props;
     const [count, setCount]= useState(0);
-    const [delta, setDelta] = useState(1);        
+       
     function incr(){
         
         setCount(
@@ -13,15 +14,12 @@ function Counter() {
     function reset(){
         setCount(0) 
     }
-    function handleDelta(event){
-        console.log(event)
-        setDelta(Number(event.target.value))
-    }   
+
     
     return (
       <div>
        <h1>Counter</h1>
-       <p><input type="number" value = {delta} onChange= {handleDelta}/></p>
+       
        <p><div class="button-29">{count}</div></p>
        <button class="button-29"  onClick={incr}>Click to add {delta} to counter.</button> <p></p>
        <button class="button-29"  onClick={reset}>Reset counter</button>
