4fa36eace2bc95672a731538e008134f3f158da4
Daniel Oliel <danielol@post.jce.ac.il>
2022-11-25 10:35:46 +0200

    Roni solved homework2

--- usestate/src/Counter.js
+++ usestate/src/Counter.js
@@ -2,27 +2,23 @@ import { useState } from "react";
 function Counter() {
     const [count, setCount]= useState(0);
     function incr(){
+        
         setCount(
             function(oldCount){
-                return oldCount +1;
+                 return oldCount +1;
             }
-        )
-        console.log(count)
+        )   
     }
-    function clear(){
-        setCount(
-            function(oldCount){
-                oldCount =0;
-                return oldCount;
-            }
-        )
-    }
+    function reset(){
+        setCount(0) }
+        
+    
     return (
       <div>
        <h1>Counter</h1>
        <p><div class="button-29">{count}</div></p>
        <button class="button-29"  onClick={incr}>Click to add 1 to counter.</button> <p></p>
-       <button class="button-29"  onClick={clear}>Click to reset the counter</button>
+       <button class="button-29"  onClick={reset}>Reset counter</button>
       </div>
     );
   }
