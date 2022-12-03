40768458c11fe698d09c1467978b17cbc9254251
Daniel Oliel <danielol@post.jce.ac.il>
2022-12-03 22:57:16 +0200

    targil4

--- usestate/src/App.js
+++ usestate/src/App.js
@@ -6,20 +6,30 @@ function App() {
   const [delta, setDelta] = useState(1); 
   const [maxNum, setMaxNum]= useState(10);
   const [reset, setReset] = useState(false);
+  const [maxVal, setMaxVal] = useState(0); 
+  const [maxValLogicChange, setMaxValLogicChange] = useState(false);
 function handleDelta(event){
   setDelta(Number(event.target.value));
   
 }
 function handleMaxNum(event){
+
+
   setMaxNum(Number(event.target.value));
 }
 
 function getReset(data){
    console.log(data);
-   setReset(data);
+   setReset(data)
+   ;
 }
-
-
+function getMaxVal(data){
+  console.log(data);
+  setMaxValLogicChange(data);
+}
+function UpdateMaxVal(val){
+  setMaxVal(val);
+}
 
   return (
     <div className="App">
@@ -28,9 +38,11 @@ function getReset(data){
      <p><input className='inputcss' type="number" value = {delta} onChange= {handleDelta}/></p>
      <p><h3>Enter maximum number:</h3></p>
      <p><input className='inputcss' type="number" value = {maxNum} onChange= {handleMaxNum}/></p>
-      
-     <Counter delta= {delta} maxNum= {maxNum} getReset= {getReset} needToReset= {reset}/>
-     <Counter delta= {delta} maxNum= {maxNum} getReset= {getReset} needToReset= {reset}/>
+      <p><h3>Maximum value: {maxVal} </h3></p>  
+     <Counter delta= {delta} maxNum= {maxNum} getReset= {getReset} needToReset= {reset} 
+       getMaxVal={getMaxVal} needMaxValue= {maxValLogicChange} UpdateMaxVal={UpdateMaxVal} maxVal= {maxVal}/>
+     <Counter delta= {delta} maxNum= {maxNum} getReset= {getReset} needToReset= {reset}
+     getMaxVal={getMaxVal} needMaxValue= {maxValLogicChange} UpdateMaxVal={UpdateMaxVal} maxVal= {maxVal}/>
     </div>
   );
 }
--- usestate/src/Counter.js
+++ usestate/src/Counter.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 function Counter(props) {
     const {delta, maxNum, getReset, needToReset} = props;
+    const {getMaxVal,needMaxValue,maxVal,UpdateMaxVal} = props;
     const [count, setCount]= useState(0);
  
     useEffect(()=>{
@@ -10,6 +11,15 @@ function Counter(props) {
       }
     },[needToReset, getReset])
 
+    useEffect(()=>{
+      if(needMaxValue){
+        UpdateMaxVal(delta +maxVal)
+        getMaxVal(false)
+        
+               
+      }
+    },[needMaxValue,getMaxVal,UpdateMaxVal,delta,maxVal])
+
     function incr(){
         
         setCount(
@@ -18,17 +28,30 @@ function Counter(props) {
                  return oldCount +delta;
                  }
                 else{ return 0}
-        }
-        )   
+        },
+        )
+      
     }
+    
     function reset(){
         getReset(true)
-        }  
+        } 
+     function getMaxVale(){   
+          getMaxVal(true);
+       }
+      
+      
+    function together(){
+      incr();
+      getMaxVale();
+    }
+   
+
     return (
       <div>
        <h1>Counter</h1>
        <p><div class="button-29">{count}</div></p>
-       <button class="button-29"  onClick={incr}>Click to add {delta} to counter.</button> <p></p>
+       <button class="button-29"  onClick= {together}>  Click to add {delta} to counter.</button> <p></p>
        <button class="button-29"  onClick={reset}>Reset counter</button>
       </div>
     );
