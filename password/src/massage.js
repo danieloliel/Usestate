d14814a12ceae4cf7ab375357987b648ab3187db
Daniel Oliel <danielol@post.jce.ac.il>
2022-12-10 13:35:37 +0200

    targil5 update

--- password/src/massage.js
+++ password/src/massage.js
@@ -1,13 +1,23 @@
+import { useState } from "react";
 
 function Massage(props) {
   
   const {input} = props;
+  const[Strange,setStrange] = useState(0);
 
-  
+  function chackStrange(){
+    if(input.length <=2){
+      setStrange("Short")
+    }
+    else if(input.length <=6){
+      setStrange("Medium")
+    }
+    else{setStrange("Strong")}
+  }
   return (
     <div>
-     <h3>Password {input.length > 6 ? <p>Password is ok</p> : <p>Too short</p>}</h3>
-   
+    <h3><button className="button-29" onClick={chackStrange} >click</button> </h3>
+    <p>Password {Strange}</p>
     </div>
   );
 }
